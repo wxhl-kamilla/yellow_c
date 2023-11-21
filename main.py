d21cf2ac59c4073ad7154999e999056c20f76417
wxhl-kamilla <wxhl8@yandex.tu>
2023-11-21 17:04:17 +0300

    added ui2

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('sth')
+import sys
+
+from PyQt5 import uic  # Импортируем uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('01.ui', self)  # Загружаем дизайн
+        self.pushButton.clicked.connect(self.run)
